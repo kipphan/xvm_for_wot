dfc1c1487c3ae9c0c7a28de94d619e701af6a7cb
Domenic Kipphan <dev@domenic-kipphan.de>
2019-10-24 18:23:42 +0200

    Update XVM 8.1.3 - custom

--- source/res_mods/mods/xfw_packages/xvm_battle/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_battle/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.1.2'
+__xvm_version__ = '8.1.3'
 __wot_version__ = '1.6.1.1'
-__revision__ = '23'
+__revision__ = '7'
 __branch__ = 'master'
-__node__ = 'bdfd9605c8051c3592016e3e60ff96e7e51bd9c6'
+__node__ = 'c23d30763ff5f460ab71e98afb52440bf2eb89ac'
 __development__ = 'False'
