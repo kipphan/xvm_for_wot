a34882aedd19a8061009b60302422a333811c2fb
Domenic Kipphan <dev@domenic-kipphan.de>
2019-10-24 18:19:47 +0200

    Update XVM 8.1.5
      [GENERAL]
      * Fixed XVM loading if WoT path has non-latin characters
      * Initial macOS Catalina support

--- source/res_mods/mods/xfw_packages/xvm_profile/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_profile/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.1.3'
-__wot_version__ = '1.6.1.1'
+__xvm_version__ = '8.1.5'
+__wot_version__ = '1.6.1.2'
 __revision__ = '7'
 __branch__ = 'master'
-__node__ = 'c23d30763ff5f460ab71e98afb52440bf2eb89ac'
+__node__ = '69bec6eb6b9fca2c85ed0ecda99fc568dc1b7264'
 __development__ = 'False'
