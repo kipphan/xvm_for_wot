9737320fbb8cf58f5ee499bb7148812a9ab7f62e
Domenic Kipphan <dev@domenic-kipphan.de>
2019-11-19 18:35:28 +0100

    Update XVM 8.1.8 - custom
      [GENERAL]
      * World of Tanks 1.6.1.4
    
      [HANGAR]
      * Renamed options in the "hangar"/"equipmentPanel" section:
          "showNumberEquipment"  -> "showConsumablesCount"
          "showNumberDirectives" -> "showDirectivesCount"
    
      [BARRACKS]
      * Added new feature of sorting tankmen in barracks from config (hangar.xc)
      * Added section:
          "hangar"/"barracks"
    
      [CAROUSEL]
      * The functionality in the "carousel"/"filters" section has been restored
      * Added options:
          "carousel"/"filters"/"elite"/"enabled" - show/hide "Elite vehicle" filter
          "carousel"/"filters"/"premium"/"enabled" - show/hide "Premium vehicle" filter
    
      [HITLOG]
      * Added macro:
          {{number}} - line number

--- source/res_mods/mods/xfw_packages/xvm_battle/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_battle/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.1.7'
-__wot_version__ = '1.6.1.3'
-__revision__ = '46'
+__xvm_version__ = '8.1.8'
+__wot_version__ = '1.6.1.4'
+__revision__ = '42'
 __branch__ = 'master'
-__node__ = '13d4d56d07ce731859705e278a0bf3c7ded8c432'
+__node__ = '1d7cebc9bbbece2423448d2c28619aec0162b9b2'
 __development__ = 'False'
