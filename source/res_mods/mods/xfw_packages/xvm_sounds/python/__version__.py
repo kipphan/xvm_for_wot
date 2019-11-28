919bc1ce6f90967aae328f0f9cfca494c95c4e0f
Domenic Kipphan <dev@domenic-kipphan.de>
2019-11-28 17:49:55 +0100

    Update XVM 8.1.9
      * World of Tanks 1.6.1.4 micropatch 2019.11.27

--- source/res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.1.8'
+__xvm_version__ = '8.1.9'
 __wot_version__ = '1.6.1.4'
-__revision__ = '42'
-__branch__ = 'master'
-__node__ = '1d7cebc9bbbece2423448d2c28619aec0162b9b2'
+__revision__ = '3'
+__branch__ = 'wot-1.6.1.4'
+__node__ = '12fae59bcec18f209b51c60aa5932f39d64c182f'
 __development__ = 'False'
