8c45263cad6d5aa9396a99b67d5465f44d421fe1
Domenic Kipphan <dev@domenic-kipphan.de>
2019-12-19 16:35:19 +0100

    Update XVM 8.2.1
      [KNOWN ISSUES]
      * No indicator of the player's name hiding enabled in the players panel
    
      [GENERAL]
      * World of Tanks 1.7.0.2
      * Various bugs fixed

--- source/res_mods/mods/xfw_packages/xvm_limits/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_limits/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.2.0'
-__wot_version__ = '1.7.0.1'
-__revision__ = '43'
+__xvm_version__ = '8.2.1'
+__wot_version__ = '1.7.0.2'
+__revision__ = '21'
 __branch__ = 'master'
-__node__ = '3edea2cee5e13ac4787e3de3b04b891576ff489c'
+__node__ = '37b8de985d69765076866b23239db6c12e98818c'
 __development__ = 'False'
