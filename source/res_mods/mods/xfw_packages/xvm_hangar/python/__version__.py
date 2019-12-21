04f116b26856e206ff28fe12880e10e90d9a5a75
Domenic Kipphan <dev@domenic-kipphan.de>
2019-12-21 21:56:29 +0100

    Update XVM 8.2.3
      [GENERAL]
      * Fix fonts loading

--- source/res_mods/mods/xfw_packages/xvm_hangar/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_hangar/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.2.2'
+__xvm_version__ = '8.2.3'
 __wot_version__ = '1.7.0.2'
-__revision__ = '24'
+__revision__ = '3'
 __branch__ = 'master'
-__node__ = '1e5264139f350fff14642924c192f648e625c715'
+__node__ = '0fcb77cdc61c93684e57a7e44d8caaff99ab3761'
 __development__ = 'False'
