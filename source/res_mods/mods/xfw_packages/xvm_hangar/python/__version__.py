a4149cbd9a97f6687f288a1a32f43461e042d6b5
Domenic Kipphan <dev@domenic-kipphan.de>
2020-03-17 20:14:47 +0100

    Update XVm 8.3.5 - custom

--- source/res_mods/mods/xfw_packages/xvm_hangar/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_hangar/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.3.2'
-__wot_version__ = '1.8.0.0'
-__revision__ = '43'
+__xvm_version__ = '8.3.5'
+__wot_version__ = '1.8.0.1'
+__revision__ = '4'
 __branch__ = 'master'
-__node__ = '29bb8ab705b6b4664ed3683a6e588fd36e4bfba4'
+__node__ = 'fb2db8b839585c9cf1832381d9110497b7aec4e4'
 __development__ = 'False'
