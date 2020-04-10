4acaa65ad65a735acf113f75c208bbcbbfb6b612
Domenic Kipphan <dev@domenic-kipphan.de>
2020-04-10 20:38:32 +0200

    Update XVM 8.4.0 - custom
      [GENERAL]
      * World of Tanks 1.8.0.2

--- source/res_mods/mods/xfw_packages/xvm_tankcarousel/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_tankcarousel/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.3.5'
-__wot_version__ = '1.8.0.1'
-__revision__ = '4'
+__xvm_version__ = '8.4.0'
+__wot_version__ = '1.8.0.2'
+__revision__ = '0021'
 __branch__ = 'master'
-__node__ = 'fb2db8b839585c9cf1832381d9110497b7aec4e4'
+__node__ = '1a68238bcefcc12f47ddea09a975c20eda223a5a'
 __development__ = 'False'
