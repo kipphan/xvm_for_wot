758b84cfd91c9ec2819fbf9e5e92b9ad28c27231
Domenic Kipphan <dev@domenic-kipphan.de>
2020-05-01 18:18:03 +0200

    Update XVM 8.5.0 - custom
    XVM-8.5.0:
      [GENERAL]
      * World of Tanks 1.9.0.1
    
      [HANGAR]
      * added option:
          "hangar"/"showTenYearsWidget" - show widget "World of Tanks' 10th Anniversary" in the hangar

--- source/res_mods/mods/xfw_packages/xvm_squad/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_squad/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.4.0'
-__wot_version__ = '1.8.0.2'
-__revision__ = '0021'
+__xvm_version__ = '8.5.0'
+__wot_version__ = '1.9.0.1'
+__revision__ = '0033'
 __branch__ = 'master'
-__node__ = '1a68238bcefcc12f47ddea09a975c20eda223a5a'
+__node__ = 'c292670cb47edb311f10cdd8c836d522b0640b04'
 __development__ = 'False'
