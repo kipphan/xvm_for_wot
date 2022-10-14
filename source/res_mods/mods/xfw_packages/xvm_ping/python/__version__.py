486321605fc1a15ba3663c159127b11a02bde38d
Domenic Kipphan <domenic@domenic-kipphan.de>
2022-10-14 19:02:42 +0200

    Update XVM 10.0.0
    For World of Tanks 1.18.0.3

--- source/res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '9.3.4'
-__wot_version__ = '1.17.1.3'
-__revision__ = '0002'
+__xvm_version__ = '10.0.0'
+__wot_version__ = '1.18.0.3'
+__revision__ = '0055'
 __branch__ = 'master'
-__node__ = 'bd41812adb34cef1a4fced7b4361447f31c3f7d8'
+__node__ = '2cc175ed7af060fa0ef916bac55869a0223a0779'
 __development__ = 'False'
