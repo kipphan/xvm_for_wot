9ec8c67099312850ee5547e8b8c14a01d76f3745
Domenic Kipphan <domenic@domenic-kipphan.de>
2023-01-16 18:15:06 +0100

    Update XVM 10.3.0
    For World of Tanks 1.19.1.0

--- source/res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '10.2.1'
-__wot_version__ = '1.19.0.2'
-__revision__ = '0000'
+__xvm_version__ = '10.3.0'
+__wot_version__ = '1.19.1.0'
+__revision__ = '0007'
 __branch__ = 'master'
-__node__ = 'f877b1facbe973433f1c64bbe44db46d10aa77a0'
+__node__ = '0f13071091c08a232fcb71ce4db3b6ea6f9617b8'
 __development__ = 'False'
