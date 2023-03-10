afa224b1f3de23481530d3be3493d040180888c6
Domenic Kipphan <domenic@domenic-kipphan.de>
2023-03-10 19:55:20 +0100

    Update XVM 10.4.0
    For World of Tanks 1.20.0.0

---
+++ source/res_mods/mods/xfw_packages/xvm_battle_crosshair/python/__version__.py
@@ -0,0 +1,8 @@
+# This file was created automatically from build script
+__xvm_version__ = '10.4.0'
+__wot_version__ = '1.20.0.0'
+__revision__ = '0000'
+__branch__ = 'master'
+__node__ = '480c44bee412e838723209ac873279a7bafd58e5'
+__development__ = 'False'
+__flavor__ = 'wg'
--- source/res_mods/mods/xfw_packages/xvm_export/python/__version__.py
+++
@@ -1,7 +0,0 @@
-# This file was created automatically from build script
-__xvm_version__ = '10.4.0'
-__wot_version__ = '1.20.0.0'
-__revision__ = '0007'
-__branch__ = 'master'
-__node__ = 'c9952bc834c43a877d8facf42947f939903d2959'
-__development__ = 'True'
