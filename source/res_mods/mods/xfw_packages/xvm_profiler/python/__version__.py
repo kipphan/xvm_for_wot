4db5a9d23a19fa68dc804a56c1f54852695fa493
Domenic Kipphan <dev@domenic-kipphan.de>
2025-04-16 19:44:38 +0200

    XVM 12.3.0

--- source/res_mods/mods/xfw_packages/xvm_profiler/python/__version__.py
+++ source/res_mods/mods/xfw_packages/xvm_profiler/python/__version__.py
@@ -1,7 +1,7 @@
 # This file was created automatically from build script
-__xvm_version__ = '12.2.0'
-__wot_version__ = '1.28.0.1'
-__revision__ = '0015'
+__xvm_version__ = '12.3.0'
+__wot_version__ = '1.28.1.0'
+__revision__ = '0011'
 __branch__ = 'master'
-__node__ = 'b3c54dcb8fb0709f9bff0c808b4ca8e724a12739'
+__node__ = '7d70ac0a9a79fed1846e1f6752db0a8882578c1a'
 __flavor__ = 'wg'
